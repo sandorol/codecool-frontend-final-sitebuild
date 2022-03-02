b080676d771786b733e648537209a23a9f455e47
sandoro <osanyi85@gmail.com>
2022-03-02 12:55:53 +0100

    Sitebuild almost done

---
+++ script.js
@@ -0,0 +1,22 @@
+const beers = [
+
+{
+    title: "Mango Bay",
+    sub: "Mad Scientist Beer",
+    text: "Pale Ale"
+},
+
+{
+    title: "Távoli Galaxis",
+    sub: "Rothbeer Brewery",
+    text: "IPA - American"
+},
+
+{
+    title: "Flying Rabbit AIPA",
+    sub: "MONYO Brewing Co.",
+    text: "IPA - American"
+}
+
+
+]
